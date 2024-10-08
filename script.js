e311a9ea327b94c8c28ef6d894896ca4cbea28d8
Aditya Singh <eradityasingh0786@gmail.com>
2024-10-08 11:57:16 +0530

    question div

--- script.js
+++ script.js
@@ -53,5 +53,66 @@ function slideChange(count){
     }
 
 }
-window.onload=slideChange('one');
+function questionSlideChange(count){
+    var elementOne=document.getElementById("question-slide-one");
+    var elementTwo=document.getElementById("question-slide-two");
+    var elementThree=document.getElementById("question-slide-three");
+    var elementFour=document.getElementById("question-slide-four");
+    var elementFive=document.getElementById("question-slide-five");
+    var elementSix=document.getElementById("question-slide-six");
+    if(count=="one"){
+        elementOne.classList.add("question-slide-active");
+        elementTwo.classList.remove("question-slide-active");
+        elementThree.classList.remove("question-slide-active");
+        elementFour.classList.remove("question-slide-active");
+        elementFive.classList.remove("question-slide-active");
+        elementSix.classList.remove("question-slide-active");
+    }
+    else if(count=="two"){
+        elementOne.classList.remove("question-slide-active");
+        elementTwo.classList.add("question-slide-active");
+        elementThree.classList.remove("question-slide-active");
+        elementFour.classList.remove("question-slide-active");
+        elementFive.classList.remove("question-slide-active");
+        elementSix.classList.remove("question-slide-active");
+    }
+    else if(count=="three"){
+        elementOne.classList.remove("question-slide-active");
+        elementTwo.classList.remove("question-slide-active");
+        elementThree.classList.add("question-slide-active");
+        elementFour.classList.remove("question-slide-active");
+        elementFive.classList.remove("question-slide-active");
+        elementSix.classList.remove("question-slide-active");
+    }
+    else if(count=="four"){
+        elementOne.classList.remove("question-slide-active");
+        elementTwo.classList.remove("question-slide-active");
+        elementThree.classList.remove("question-slide-active");
+        elementFour.classList.add("question-slide-active");
+        elementFive.classList.remove("question-slide-active");
+        elementSix.classList.remove("question-slide-active");
+    }
+    else if(count=="five"){
+        elementOne.classList.remove("question-slide-active");
+        elementTwo.classList.remove("question-slide-active");
+        elementThree.classList.remove("question-slide-active");
+        elementFour.classList.remove("question-slide-active");
+        elementFive.classList.add("question-slide-active");
+        elementSix.classList.remove("question-slide-active");
+    }
+    else if(count=="six"){
+        elementOne.classList.remove("question-slide-active");
+        elementTwo.classList.remove("question-slide-active");
+        elementThree.classList.remove("question-slide-active");
+        elementFour.classList.remove("question-slide-active");
+        elementFive.classList.remove("question-slide-active");
+        elementSix.classList.add("question-slide-active");
+    }
+
+
+}
+window.onload=function(){
+    slideChange('one');
+    questionSlideChange('one');
+}
 
